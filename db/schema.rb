e37ba443d5abd569bfa008811c8d4750fb91a0a0
monizumarraga <monizumarraga.hotmail.com>
2018-08-01 06:27:15 +0000

    table user article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_062100) do
+ActiveRecord::Schema.define(version: 2018_08_01_062613) do
 
   create_table "articles", force: :cascade do |t|
     t.string "code"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2018_08_01_062100) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_articles", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_user_articles_on_article_id"
+    t.index ["user_id"], name: "index_user_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
